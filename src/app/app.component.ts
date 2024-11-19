1d2eb67814f208493ad5f084c10e62f5487def06
oartuu <artu.donascimento@gmail.com>
2024-11-18 22:18:47 -0300

    feat: add icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {LucideAngularModule, Notebook, Library, ArrowRight} from "lucide-angular";
-
-
+//import { LucideIconProvider } from 'lucide-angular';
+//import {LucideAngularModule, Notebook, Library, ArrowRight} from "lucide-angular";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBars, faBook, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LucideAngularModule ],
+  imports: [RouterOutlet, FontAwesomeModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'extensao';
+  faBars = faBars;
+  faBook = faBook;
+  faArrowRight = faArrowRight;
 }
