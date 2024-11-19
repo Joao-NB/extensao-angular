c443416ab4caae78391fc316876e3b29961a254a
João-NB <nbjoaoguilherme@gmail.com>
2024-11-18 23:09:57 -0300

    Terceiro commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
 
+  onLogin(): void {
+    console.log('E-mail:', this.email);
+    console.log('Senha:', this.password);
+  }
 }
